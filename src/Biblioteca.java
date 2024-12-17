615a7c02febbfd92b150415b1b0082b8871da0b2
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-17 17:13:01 -0300

    fix:Adiciona getter isDisponivel() na classe Livro para corrigir acesso ao campo 'disponivel' na classe Biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -9,23 +9,64 @@ public class Biblioteca {
         this.usuarios = new ArrayList<>();
     }
 
-    public void cadastroUsuario(Usuario usuario){
+    public void cadastroLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void cadastroUsuario(Usuario usuario) {
         usuarios.add(usuario);
     }
 
     public void realizarEmprestimo(String isbn, int idUsuario) {
         Livro livro = buscarLivroPorIsbn(isbn);
-        Usuario = usuario = buscarUsuarioPorId(idUsuario);
+        Usuario usuario = buscarUsuarioPorId(idUsuario);
+
+        if (livro != null && usuario != null && livro.isDisponivel() && usuario.livrosEmprestados.size() < 3) {
+            livro.emprestar();
+            usuario.adicionarLivro(livro);
+            System.out.println("Empréstimo realizado com sucesso!");
+        } else {
+            System.out.println("Empréstimo não pode ser realizado.");
+        }
+    }
+
+    public void realizarDevolucao(String isbn, int idUsuario) {
+        Livro livro = buscarLivroPorIsbn(isbn);
+        Usuario usuario = buscarUsuarioPorId(idUsuario);
 
         if (livro != null && usuario != null && usuario.livrosEmprestados.contains(livro)) {
             livro.devolver();
             usuario.removerLivro(livro);
-
             System.out.println("Devolução realizada com sucesso!");
         } else {
             System.out.println("Devolução não pode ser realizada.");
         }
     }
 
+    public void exibirLivrosDisponiveis() {
+        for (Livro livro : livros) {
+            // Usando o getter isDisponivel() para acessar a disponibilidade do livro
+            if (livro.isDisponivel()) {
+                livro.exibirDetalhes();
+            }
+        }
+    }
 
+    private Livro buscarLivroPorIsbn(String isbn) {
+        for (Livro livro : livros) {
+            if (livro.isbn.equals(isbn)) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    private Usuario buscarUsuarioPorId(int id) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.id == id) {
+                return usuario;
+            }
+        }
+        return null;
+    }
 }

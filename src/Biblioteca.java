3357e64039af9e2648a7edec284e338797e06590
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-17 16:25:41 -0300

    feat: cria classe Biblioteca com atributos livros e usuarios

---
+++ src/Biblioteca.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Biblioteca {
+    public ArrayList<Livro> livros;
+    public ArrayList<Usuario> usuarios;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+        this.usuarios = new ArrayList<>();
+    }
+}

4a85c0fbee283a55a6017476755f1595d4c33be4
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-17 17:06:49 -0300

    Fix: Corrige erro na classe Usuario na exibição de detalhes e adiciona método removerLivro

--- src/Usuario.java
+++ src/Usuario.java
@@ -11,12 +11,23 @@ public class Usuario {
         this.livrosEmprestados = new ArrayList<>();
     }
 
-    public void adicionarLivro (Livro livro) {
+    public void adicionarLivro(Livro livro) {
         if (livrosEmprestados.size() < 3) {
             livrosEmprestados.add(livro);
+        } else {
+            System.out.println("Limite de livros emprestados atingido.");
         }
     }
-        public void exibirDetalhes(){
-                System.out.println("Nome: " +nome + ", ID: " +id + "Livros Emprestados: " + livrosEmprestados.size());
+
+    public void removerLivro(Livro livro) {
+        if (livrosEmprestados.contains(livro)) {
+            livrosEmprestados.remove(livro);
+        } else {
+            System.out.println("Este livro não está na lista de livros emprestados.");
         }
     }
+
+    public void exibirDetalhes() {
+        System.out.println("Nome: " + nome + ", ID: " + id + " Livros Emprestados: " + livrosEmprestados.size());
+    }
+}

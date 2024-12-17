9ea8f2daad0c17c1d6d8adc1f1d4acdb42c146f6
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-17 16:36:01 -0300

    feat: adiciona métodos de cadastro de usuário e realização de empréstimo na classe Biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -8,4 +8,24 @@ public class Biblioteca {
         this.livros = new ArrayList<>();
         this.usuarios = new ArrayList<>();
     }
+
+    public void cadastroUsuario(Usuario usuario){
+        usuarios.add(usuario);
+    }
+
+    public void realizarEmprestimo(String isbn, int idUsuario) {
+        Livro livro = buscarLivroPorIsbn(isbn);
+        Usuario = usuario = buscarUsuarioPorId(idUsuario);
+
+        if (livro != null && usuario != null && usuario.livrosEmprestados.contains(livro)) {
+            livro.devolver();
+            usuario.removerLivro(livro);
+
+            System.out.println("Devolução realizada com sucesso!");
+        } else {
+            System.out.println("Devolução não pode ser realizada.");
+        }
+    }
+
+
 }

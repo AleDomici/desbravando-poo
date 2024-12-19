b6449cb0864f34bb88b0f56c0b9311fc8578fb46
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-19 11:32:20 -0300

    style(livro): reorganiza o código com os principios clean code

--- src/Livro.java
+++ src/Livro.java
@@ -1,31 +1,63 @@
 
 public class Livro {
-    String titulo;
-    String autor;
-    String isbn;
-    private boolean disponivel;
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean isDisponivel;
 
     public Livro(String titulo, String autor, String isbn){
         this.titulo = titulo;
         this.autor = autor;
         this.isbn = isbn;
-        this.disponivel = true;
+        this.isDisponivel = true;
+    }
+    public void exibirDetalhes(){
+        System.out.println("Título: " +titulo);
+        System.out.println("Autor: " +autor);
+        System.out.println("ISBN: " +isbn);
+        System.out.println("Disponível: " + (isDisponivel ? "Sim" : "Não"));
     }
 
     public void emprestar() {
-        if (disponivel){
-            disponivel= false;
+        if (isDisponivel){
+            isDisponivel = false;
+        } else {
+            System.out.println("O livro já está emprestado.");
         }
     }
 
     public void devolver () {
-        disponivel = true;
-    }
-    public boolean isDisponivel(){
-        return disponivel;
+        if(!isDisponivel) {
+            isDisponivel = true;
+            System.out.println("O livro foi devolvido com sucesso.");
+        } else{
+            System.out.println("O livro já está disponível");
+        }
     }
 
-    public void exibirDetalhes(){
-        System.out.println("Título: " +titulo + "Autor: " +autor + "ISBN: " +isbn + "Disponibilidade: " +disponivel);
+    private boolean isDisponivel() {
+        return isDisponivel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 }

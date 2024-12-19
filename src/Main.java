3b307e36640981ca768361e22579b34d5862137f
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-19 15:55:19 -0300

    refactor: melhora a estrutura do menu e adiciona métodos auxiliares  - Remove a importação desnecessária de `javax.sound.midi.Soundbank`   - Adiciona métodos auxiliares para modularizar as operações do menu (`cadastrarLivro`, `cadastrarUsuario`, `realizarEmprestimo`, `realizarDevolucao`, `exibirLivrosDisponiveis`).    - Melhora a exibição do menu com uma mensagem mais clara para o usuário.    - Corrige a duplicação de lógica ao reutilizar métodos auxiliares.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import javax.sound.midi.Soundbank;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -38,18 +36,70 @@ public class Main {
                      System.out.println("Encerrando o sistema. Até logo!");
                     scanner.close();
                     return;
+
                  default:
                      System.out.println("Opção inválida. Tente novamente.");
              }
         }
     }
+
     private static void exibirMenu() {
-        System.out.println("Bem-vindo ao Sistema de Gerenciamento de Biblioteca! Escolha uma opção: ");
+        System.out.println("Bem-vindo ao Sistema de Gerenciamento de Biblioteca!");
         System.out.println("1. Cadastrar livro");
         System.out.println("2. Cadastrar usuário");
         System.out.println("3. Realizar empréstimo");
         System.out.println("4. Realizar devolução");
         System.out.println("5. Exibir livros disponíveis");
         System.out.println("6. Sair");
+
+        System.out.print("Escolha uma opção: ");
+    }
+
+    private static void cadastrarLivro(Scanner scanner, Biblioteca biblioteca) {
+        System.out.println("Digite o título do livro: ");
+        String titulo = scanner.nextLine();
+
+        System.out.println("Digite o autor do livro: ");
+        String autor = scanner.nextLine();
+
+        System.out.println("Digite o  ISBN do livro: ");
+        String isbn = scanner.nextLine();
+
+        Livro livro = new Livro(titulo, autor, isbn);
+        biblioteca.cadastrarLivro(livro);
+        System.out.println("Livro cadastrado com sucesso!");
+    }
+
+    private static void cadastrarUsuario(Scanner scanner, Biblioteca biblioteca) {
+        System.out.println("Digite o nome do usuário: ");
+        String nome = scanner.nextLine();
+        System.out.println("Digite ID do usuário: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        Usuario usuario = new Usuario(nome, id);
+        biblioteca.cadastrarUsuario(usuario);
+        System.out.println("Usuário cadastrado com sucesso!");
+    }
+
+    private static void realizarEmprestimo(Scanner scanner, Biblioteca biblioteca) {
+        System.out.println("Digite ISBN do livro: ");
+        String isbn = scanner.nextLine();
+        System.out.println("Digite o ID do usuário: ");
+        int idUsuario = Integer.parseInt(scanner.nextLine());
+
+        biblioteca.realizarEmprestimo(isbn,idUsuario);
+    }
+
+    private static void realizarDevolucao(Scanner scanner, Biblioteca biblioteca) {
+        System.out.println("Digite o ISBN do livro: ");
+        String isbn = scanner.nextLine();
+        System.out.println("Digite o ID do usuário: ");
+        int idUsuario = Integer.parseInt(scanner.nextLine());
+
+        biblioteca.realizarEmprestimo(isbn,idUsuario);
+    }
+    private static void exibirLivrosDisponiveis(Biblioteca biblioteca) {
+        biblioteca.exibirLivrosDisponiveis();
     }
 }

e81969442212f5abfdacec63b85fa0918427b5dc
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-19 13:47:25 -0300

    feat: implementar estrutura inicial do menu e métodos de cadastro de livro e usuário

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,78 +9,47 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
             //Loop While para interagir com o menu
         while (true) {
-            System.out.println("Bem-vindo(a) ao Sistema de Gerenciamento de Biblioteca!");
-            System.out.println("1. Cadastrar livro");
-            System.out.println("2. Cadastrar usuário");
-            System.out.println("3. Realizar empréstimo");
-            System.out.println("4. Realizar devolução");
-            System.out.println("5. Exibir livros disponíveis");
-            System.out.println("6. Sair");
-            System.out.println("Escolha uma opção: ");
-
+            exibirMenu();
             int opcao = scanner.nextInt();          //le a opção escolhida pelo user
              scanner.nextLine();
 
              // Bloco switch que executa uma ação conforme a opção escolhida pelo Usuário
              switch (opcao) {
                  case 1:
-                     // Cadastrar livro
-                     System.out.print("Digite o título do livro: ");
-                     String titulo = scanner.nextLine();
-                     System.out.print("Digite o autor do livro: ");
-                     String autor = scanner.nextLine();
-                     System.out.print("Digite o ISBN do livro: ");
-                     String isbn = scanner.nextLine();
-
-                        // Chama o método cadastroLivro da classe Biblioteca, passando um objeto Livro criado
-                     biblioteca.cadastroLivro(new Livro(titulo, autor, isbn));
-                     System.out.println("Livro cadastrado com sucesso!");
-
-                     break;
+                     cadastrarLivro(scanner, biblioteca);
 
                  case 2:
-                     System.out.print("Digite o nome do usuário: ");
-                     String nome = scanner.nextLine();
-                     System.out.print("Digite o ID do usuário: ");
-                     int id = scanner.nextInt();
-                     scanner.nextLine();
-
-
-                     biblioteca.cadastroUsuario(new Usuario(nome, id));
-                     System.out.println("Usuário cadastrado com sucesso!");
+                     cadastrarUsuario(scanner, biblioteca);
                      break;
 
                  case 3:
-                     System.out.println("Digite o Isbn do livro: ");
-                     String isbnEmprestimo = scanner.nextLine();
-                     System.out.println("Digite o ID do usuário: ");
-                     int idUsuarioEmprestimo = scanner.nextInt();
-                     scanner.nextLine();
-                     biblioteca.realizarEmprestimo(isbnEmprestimo, idUsuarioEmprestimo);
+                     realizarEmprestimo(scanner, biblioteca);
                      break;
 
                  case 4:
-                     System.out.println("Digite o Isbn do livro: ");
-                     String isbnDevolucao = scanner.nextLine();
-                     System.out.println("Digite o ID do usuário: ");
-                     int idUsuarioDevolucao = scanner.nextInt();
-                     scanner.nextLine();
-                     biblioteca.realizarEmprestimo(isbnDevolucao, idUsuarioDevolucao);
+                     realizarDevolucao(scanner, biblioteca);
                      break;
 
                  case 5:
-                     System.out.println("Livros disponiveis: ");
-                     biblioteca.exibirLivrosDisponiveis();
+                     exibirLivrosDisponiveis(biblioteca);
                      break;
 
                  case 6:
                      System.out.println("Encerrando o sistema. Até logo!");
-
-                 scanner.close();
-                 return;
+                    scanner.close();
+                    return;
                  default:
                      System.out.println("Opção inválida. Tente novamente.");
              }
         }
+    }
+    private static void exibirMenu() {
+        System.out.println("Bem-vindo ao Sistema de Gerenciamento de Biblioteca! Escolha uma opção: ");
+        System.out.println("1. Cadastrar livro");
+        System.out.println("2. Cadastrar usuário");
+        System.out.println("3. Realizar empréstimo");
+        System.out.println("4. Realizar devolução");
+        System.out.println("5. Exibir livros disponíveis");
+        System.out.println("6. Sair");
     }
 }

e0f2bc3e9b21c36abf8aedd3776a043312f31e69
[AleDomici] <alessandra.domiciano@zup.com.br>
2024-12-20 09:58:54 -0300

    docs(Usuario): adiciona comentários explicativos sobre os métodos de gerenciamento de livros

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
 
 public class Usuario extends Pessoa{
-
     public ArrayList<Livro> livrosEmprestados;
+
+    /**
+     * Construtor da classe Usuario.
+     * Inicializa a lista de livros emprestados e passa o nome e ID para a classe super (Pessoa).
+     *
+     * @param nome Nome do usuário
+     * @param id Identificador único do usuário
+     */
 
     public Usuario(String nome, int id) {
         super(nome, id);
         this.livrosEmprestados = new ArrayList<>();
     }
+
+    /**
+     * Sobrescreve o método exibirDetalhes da classe Pessoa para exibir informações específicas do Usuário.
+     * Exibe o nome, o ID e a quantidade de livros que o usuário tem emprestados.
+     */
 
     @Override
     public void exibirDetalhes() {
@@ -16,6 +28,12 @@ public class Usuario extends Pessoa{
         System.out.println("Livros emprestados: " + livrosEmprestados.size());
     }
 
+    /**
+     * Adiciona um livro à lista de livros emprestados, se o usuário não tiver atingido o limite de 3 livros.
+     *
+     * @param livro O livro a ser adicionado
+     */
+
     public void adicionarLivro(Livro livro) {
         if (livrosEmprestados.size() < 3) {
             livrosEmprestados.add(livro);
@@ -23,6 +41,13 @@ public class Usuario extends Pessoa{
             System.out.println("Limite de livros emprestados atingido.");
         }
     }
+
+    /**
+     * Remove um livro da lista de livros emprestados e o devolve.
+     *
+     * @param livro O livro a ser removido
+     * @return true se o livro foi removido com sucesso, false caso contrário
+     */
 
     public boolean removerLivro(Livro livro) {
             if (livrosEmprestados.contains(livro)) {
